03cfb285be7a6ca4f4d5cebcdd5bd5bf5d28a68a
simranshikha <simranshikha8@gmail.com>
2018-06-19 17:55:33 +0530

    componentadd

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "<Your_API_KEY_HERE>",
+    authDomain: "AUTH-DOMAIN-HERE",
+    databaseURL: "DATABASE-URL-HERE",
+    projectId: "PROJECT-ID-HERE",
+    storageBucket: "STORAGE-BUCKET-HERE",
+    messagingSenderId: "MESSAGING-SENDER-ID-HERE"
+ }
 };
 
 /*
